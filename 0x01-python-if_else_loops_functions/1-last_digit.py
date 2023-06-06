ad3f3162cef2b3274950e032398d0ad985bb826b
Uko Uwatt <uwattuko@gmail.com>
2023-06-06 15:48:09 +0000

    used if statement to extract last digit from an integer

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,13 +5,14 @@ if number < 0:
     last_digit = (abs(number) % 10) * -1
     end_str = "and is less than 6 and not 0"
     print(f"Last digit of {number} is {last_digit} {end_str}")
+elif number == 0:
+    last_digit = number % 10
+    print(f"Last digit of {number} is {last_digit} and is 0")
 if number > 0:
     last_digit = number % 10
     end_str = "and is greater than 5"
     if last_digit > 5:
         print(f"Last digit of {number} is {last_digit} {end_str}")
-    elif last_digit == 0:
-        print(f"Last digit of {number} is {last_digit} and is 0")
     elif last_digit < 6 and last_digit > 0:
         end_str2 = "and is less than 6 and not 0"
         print(f"Last digit of {number} is {last_digit} {end_str2}")
