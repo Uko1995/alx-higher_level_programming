d809660611b5ccf3e52d75aac08c06c404adc69c
Uko Uwatt <uwattuko@gmail.com>
2023-06-07 04:28:25 +0000

    printed a string in uppercase without isupper()

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+def uppercase(str):
+    for character in str:
+        uppercase = chr(ord(character) - 32) if 97 <= ord(character) <= 122 else character
+        print("{}".format(uppercase), end="")
+    
+    print()
