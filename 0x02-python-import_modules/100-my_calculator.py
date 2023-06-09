ba06cc61a4a10753826b7e19fecb819c4286fd9b
Uko Uwatt <uwattuko@gmail.com>
2023-06-09 09:02:33 +0000

    created a simple calculator using imported modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,22 +5,23 @@ from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
 
     args = len(argv)
-    if args <= 4:
+    if args < 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
     first = int(argv[1])
-    second = int(argv[2])
-    op = argv[2]
-    if op == '+':
+
+    second = int(argv[3])
+
+    if argv[2] == '+':
         result = add(first, second)
-    elif op == '-':
+    elif argv[2] == '-':
         result = sub(first, second)
-    elif op == '*':
+    elif argv[2] == '*':
         result = mul(first, second)
-    elif op == '/':
+    elif argv[2] == '/':
         result = div(first, second)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    print("{} {} {} = {}".format(first, op, second, result))
+    print("{} {} {} = {}".format(first, argv[2], second, result))
