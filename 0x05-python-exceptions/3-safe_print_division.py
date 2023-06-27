77f6d36378c04d49cca2d62b6cc5b5fc87fd8127
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-06-27 03:36:55 +0000

    divides 2 integers and prints the result

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,8 +2,9 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-        return result
     except ZeroDivisionError:
         return None
     finally:
         print("Inside Result: {}".format(None))
+
+    return result
