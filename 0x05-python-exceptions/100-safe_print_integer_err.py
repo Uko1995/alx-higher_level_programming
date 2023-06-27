76888e094dcc341aa1dcd07f0aa4e5efabe8b633
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-06-27 04:48:55 +0000

    prints an integer.

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,6 +5,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except Exception as err:
+    except (ValueError, TypeError) as err:
         print("Exception: {} is not an integer".format(err), file=sys.stderr)
         return False
