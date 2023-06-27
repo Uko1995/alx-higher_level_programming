edb2f826d87ebecd8fa49edb99ff4769d9094ac7
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-06-27 08:39:56 +0000

    list division by list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,17 +3,18 @@ def list_division(my_list_1, my_list_2, list_length):
     final_list = []
 
     for i in range(list_length):
+        result = 0
         try:
             result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             result = 0
-            print("division by 0")
+            print("{}".format("division by 0"))
         except (ValueError, TypeError):
             result = 0
-            print("wrong type")
+            print("{}".format("wrong type"))
         except IndexError:
             result = 0
-            print("out of range")
+            print("{}".format("out of range"))
         finally:
             final_list.append(result)
     return final_list
