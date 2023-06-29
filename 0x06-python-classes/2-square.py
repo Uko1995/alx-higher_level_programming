5f4ead913d4e38d39ca82af366931aba98a104f9
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-06-29 04:58:31 +0000

    defines a square by: (based on 1-square.py)

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -23,9 +23,9 @@ class Square:
         TypeError: if size is not an integer
         ValueError: if size is less then 0
         """
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        elif type(size) is not int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
