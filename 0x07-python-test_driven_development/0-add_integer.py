0c0794fce10df29057dec2ccf9d5fb519dc92016
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-07-07 14:27:26 +0000

    task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+module that includes function for adding integers
+'''
+
+def add_integer(a, b=98):
+    '''
+    function returns the addition of 2 integers
+
+    parameters:
+    a: integer or float
+    b: integer or float
+    '''
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    a = int(a)
+    b = int(b)
+
+    return a + b
