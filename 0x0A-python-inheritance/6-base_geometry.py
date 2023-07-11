42b4fe73814f904ab43d25ff18e7a5e1d35c2e4a
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-07-11 02:53:03 +0000

    Write a class BaseGeometry (based on 5-base_geometry.py)

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+module for task 6
+'''
+
+
+class BaseGeometry:
+    '''
+    empty class
+    '''
+    def area(self):
+        '''
+        function raises exception
+        '''
+        raise Exception("area() is not implemented")
