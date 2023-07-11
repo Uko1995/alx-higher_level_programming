9895ad80a57749b79a86849f4d8704b5841bdc38
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-07-11 02:37:39 +0000

    returns True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class ; otherwise False

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+module contains task 3
+'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''
+    checks if obj is a subclass of a_class
+    '''
+    return isinstance(obj, a_class)
