a37f0140684d1a886c7c6899580735928fba398c
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-07-13 02:32:31 +0000

    Write a function that returns the dictionary description with simple data structure

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+module for this task
+'''
+
+
+def class_to_json(obj):
+    '''
+    returns the dictionary description of obj
+    '''
+    obj.__class__
+    return obj.__dict__
