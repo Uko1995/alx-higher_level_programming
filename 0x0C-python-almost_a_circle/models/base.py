5ab44f57922cfa809714212f4ce5c809659b9ead
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-07-18 02:54:55 +0000

    correction made on base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -61,7 +61,7 @@ class Base:
 
         '''
         if cls.__name__ == "Rectangle":
-            obj = cls(1,1)
+            obj = cls(1, 1)
         elif cls.__name__ == "Square":
             obj = cls(1)
 
