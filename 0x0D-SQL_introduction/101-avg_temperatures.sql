ae43717cfc4aeadcfc1169d8aea415244cbb14fa
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-08-17 13:03:39 +0000

    task 18

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,6 @@
 -- import table and display
 -- displays the average temperature (Fahrenheit) by city ordered by temperature (descending)
-SELECT city, AVG(temperature) as avg_temp
+SELECT *, AVG(temperature) as avg_temp
 FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;
