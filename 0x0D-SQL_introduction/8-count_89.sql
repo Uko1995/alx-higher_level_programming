e1588bbf570c6a3465d9a5c04acf7f119c1f9458
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-08-17 06:02:19 +0000

    task 8

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,5 @@
 -- counts number of records
 -- displays the number of records with id = 89 in the table first_table
-SELECT COUNT(*) FROM first_tables WHERE id = 89;
+SELECT COUNT(*)
+FROM first_tables
+WHERE id = 89;
