519c0661d0c84ed1d5b0db5a74eb0f1f3d29ba96
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-08-18 04:49:15 +0000

    task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,6 @@
 -- creates a user
 -- create a mysql user "user_od_1"
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH 'user_0d_1_pwd';
+
+-- grant permissions
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
