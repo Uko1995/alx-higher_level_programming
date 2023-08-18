0602825bccfd91121dcc4db20cc795a049b26289
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-08-18 04:27:27 +0000

    task 0

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- show privileges
+--  lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost)
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_od_2@localhost;
