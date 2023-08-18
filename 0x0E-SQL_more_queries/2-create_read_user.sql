199b98d06bd9e71597558cffbe4b1844779d8705
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-08-18 04:45:22 +0000

    task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,10 +2,8 @@
 -- create database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
-USE hbtn_0d_2;
-
 -- create user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED WITH 'user_0d_2_pwd';
 
 -- grant privileges
-GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
