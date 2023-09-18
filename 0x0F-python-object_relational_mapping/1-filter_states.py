f5d3afc3024c2e6e83b9fde7575d0c456685fbb3
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-09-18 14:55:50 +0000

    task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+script that lists all states with a name starting with
+N (upper N) from the database hbtn_0e_0_usa
+'''
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    if len(sys.argv) == 4:
+        with MySQLdb.connect(host="localhost",
+                             user=username,
+                             password=password, database=database_name,
+                             port=3306) as database:
+
+            cursor = database.cursor()
+
+            query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+            cursor.execute(query)
+
+            states = cursor.fetchall()
+
+            for state in states:
+                print(state)
