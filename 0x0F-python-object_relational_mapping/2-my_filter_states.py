5ca6babdbe6bd4c82318fc45d1debff8c03ed0b3
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-09-20 05:08:15 +0000

    task 2 and 3

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 '''
-script that lists all states with a name starting with
-N (upper N) from the database hbtn_0e_0_usa
+script that lists all states from the database hbtn_0e_0_usa
 '''
 
 import MySQLdb
@@ -11,16 +10,17 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-    if len(sys.argv) == 4:
+    if len(sys.argv) == 5:
         with MySQLdb.connect(host="localhost",
                              user=username,
                              password=password, database=database_name,
-                             port=3306) as database:
+                             port=3306, ) as database:
 
             cursor = database.cursor()
 
-            query = "SELECT * FROM states WHERE name like binary 'N%' ORDER BY id ASC"
+            query = "SELECT * FROM states WHERE name='{}' ORDER BY id".format(state_name)
             cursor.execute(query)
 
             states = cursor.fetchall()
