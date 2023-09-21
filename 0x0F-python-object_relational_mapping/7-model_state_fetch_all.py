e475897ad5e26111000fcc156729f3b83c14bd43
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-09-21 01:06:25 +0000

    task 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+script that lists all State objects from the database hbtn_0e_6_usa
+'''
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        connection = f"mysql://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}"
+        engine = create_engine(connection)
+        Base.metadata.bind = engine
+        SESSION = sessionmaker(bind=engine)
+        session = SESSION()
+        states = session.query(State).order_by(State.id).all()
+
+        for state in states:
+            print(f"{state.id}: {state.name}")
