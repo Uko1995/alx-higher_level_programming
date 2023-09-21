40607eba7ca54b787abba5f4d4aa459840120182
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-09-21 00:34:13 +0000

    task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+python file that contains the class definition of a State and
+ an instance Base = declarative_base()
+'''
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    '''
+    class state that inherits from Base.
+    Attributes: id and name
+    '''
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=True)
