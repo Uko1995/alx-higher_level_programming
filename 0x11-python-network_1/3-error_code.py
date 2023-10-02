0c8e25e053c9894030d9ea25f766c4932a55a1b8
UKO UWATT <uwattuko@gmail.com>
2023-10-01 22:57:46 -0700

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,11 +5,12 @@ Python script that fetches https://alx-intranet.hbtn.io/status
 
 import urllib.request
 from sys import argv
+import urllib.error
 
 if __name__ == "__main__":
     try:
         with urllib.request.urlopen(argv[1]) as site:
             fetched = site.read().decode("utf-8")
             print(fetched)
-    except urllib.error.URLError as e:
+    except urllib.error.HTTPError as e:
         print(f"Status code: {e.code}")
