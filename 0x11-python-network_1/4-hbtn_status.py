e312ed70c3ba863c9d869a7b1c51269b7bbdfec4
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-10-02 06:06:25 +0000

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,7 @@ Python script that fetches https://alx-intranet.hbtn.io/status
 import requests
 
 if __name__ == "__main__":
-        fetched = requests.get("https://alx-intranet.hbtn.io/status")
-        print("Body response:")
-        print(f"\t- type: {type(fetched.text)}")
-        print(f"\t- content: {fetched.text}")
+    fetched = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print(f"\t- type: {type(fetched.text)}")
+    print(f"\t- content: {fetched.text}")
