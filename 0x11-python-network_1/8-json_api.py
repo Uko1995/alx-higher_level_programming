39cc025980f4f060095b33c06daa3237ad0ff9f9
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-10-03 07:08:07 +0000

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,15 +8,21 @@ import requests
 from sys import argv
 
 if __name__ == "__main_-":
-    if len(argv) != 2:
-        q = ""
-    else:
+    if len(argv) == 2:
         q = argv[1]
+    else:
+        q = ""
 
     url = "http://0.0.0.0:5000/search_user"
     data = {"q": q}
 
     response = requests.post(url, data)
     response_json = response.json()
-    if response_json and len(response_json) > 0:
-        print(f"[{response_json.get('id')}] {response_json.get('name')}")
+    try:
+        if not response_json:
+            print("No result")
+        else:
+            print(f"[{response_json.get('id')}] {response_json.get('name')}")
+
+    except requests.exception:
+        print("Not a valid JSON")
