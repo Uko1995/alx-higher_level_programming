5bef7e6d9c3a2b1c443ee894257a6db7482a1c8f
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-10-03 23:35:11 +0000

    task 8

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a DELETE request to the URL passed as the first argument
-curl -X POST -d @"$2" -H "Content-Type: application/json" "$1"
+curl -sX POST -d @"$2" -H "Content-Type: application/json" "$1"
