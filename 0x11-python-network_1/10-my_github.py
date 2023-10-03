e834cf1d21b0d70daa2a0179b5c7c66aa49c989f
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-10-03 08:32:41 +0000

    task 9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+Python script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+'''
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+   data = {"username": argv[1], "pasword": argv[2]}
+   url = "https://api.github.com/user"
+   response = requests.get(url, data)
+   ids = response.json().get('id')
+   print(ids)
