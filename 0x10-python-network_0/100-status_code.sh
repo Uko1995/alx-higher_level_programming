35d272e707a3727fc0ae682aa4a5921af73b64a4
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-12-01 04:29:30 +0000

    task 100

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Bash script that sends a DELETE request to the URL passed as the first argument
-curl -sI -o /dev/null  "$1" -w "%{http_code}"
+# curl -sI -o /dev/null  "$1" -w "%{http_code}"
+curl -w "http_code" "$1"
