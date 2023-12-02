008e8843a4a6f9cb648a42298af2804d3be7aff3
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-12-02 04:42:23 +0000

    list last 10 commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' script takes 2 arguments and lists the last 10 commits in a repository '''
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    repo_name = argv[1]
+    owner = argv[2]
+    url = f"https://api.github.com/repos/{owner}/{repo_name}/commits"
+    response = requests.get(url)
+    objects = response.json()
+    for i, obj in enumerate(objects):
+        print(f"{obj.get('sha')}: {obj.get('commit').get('author').get('name')}")
+        if i == 9:
+            break
